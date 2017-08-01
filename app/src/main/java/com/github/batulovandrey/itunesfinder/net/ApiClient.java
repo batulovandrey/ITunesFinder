888eb736202344d017d6a974c73d837e41dc02e8
Andrey Batulov <batulov.andrey.89@gmail.com>
2017-08-01 22:04:45 +0300

    created ApiClient and Service

---
+++ app/src/main/java/com/github/batulovandrey/itunesfinder/net/ApiClient.java
@@ -0,0 +1,25 @@
+package com.github.batulovandrey.itunesfinder.net;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+/**
+ * Created by batul0ve on 01.08.2017.
+ */
+
+public class ApiClient {
+    private static final String BASE_URL = "https://itunes.apple.com/";
+    private static Retrofit sRetrofit = null;
+
+    private ApiClient() { throw new IllegalStateException("Can't create object"); }
+
+    public static Retrofit getRetrofit() {
+        if (sRetrofit == null) {
+            sRetrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(JacksonConverterFactory.create())
+                    .build();
+        }
+        return sRetrofit;
+    }
+}
---
+++ app/src/main/java/com/github/batulovandrey/itunesfinder/net/TrackService.java
@@ -0,0 +1,16 @@
+package com.github.batulovandrey.itunesfinder.net;
+
+import com.github.batulovandrey.itunesfinder.bean.TrackResponse;
+
+import retrofit2.Call;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+/**
+ * Created by batul0ve on 01.08.2017.
+ */
+
+public interface TrackService {
+    @POST("search")
+    Call<TrackResponse> getTracks(@Query("term") String keyWord);
+}
