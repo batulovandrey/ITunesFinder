a6c36f944c803e38ab8f8d45ef1d4f10aec6f222
Andrey Batulov <batulov.andrey.89@gmail.com>
2017-08-01 14:17:34 +0300

    added fragment to show content and implemented fragment interaction with activity

--- app/src/main/java/com/github/batulovandrey/itunesfinder/MainActivity.java
+++ app/src/main/java/com/github/batulovandrey/itunesfinder/MainActivity.java
@@ -3,14 +3,17 @@ package com.github.batulovandrey.itunesfinder;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener {
     private Toolbar mToolbar;
 
     @Override
@@ -48,11 +51,25 @@ public class MainActivity extends AppCompatActivity {
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
             // TODO: 01.08.2017 use query for api
+            startFragment(query);
         }
     }
 
     private void initToolbar() {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+    }
+
+    private void startFragment(String query) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.container, MainFragment.newInstance(query, null))
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(String string) {
+        Toast.makeText(getApplicationContext(), "here we are " + string, Toast.LENGTH_SHORT).show();
     }
 }
